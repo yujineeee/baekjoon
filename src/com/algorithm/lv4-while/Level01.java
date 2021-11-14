022c45a750eb6973fa57a892146b0c6f6a637a3d
yujineeee <jangy100@nate.com>
2021-11-14 17:17:14 +0900

    lv04-while

---
+++ src/com/algorithm/lv4-while/Level01.java
@@ -0,0 +1,36 @@
+package com.algorithm.lv4-while;
+/*
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+입력
+입력은 여러 개의 테스트 케이스로 이루어져 있다.
+
+각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+입력의 마지막에는 0 두 개가 들어온다.
+
+출력
+각 테스트 케이스마다 A+B를 출력한다.
+*/
+
+import java.util.Scanner;
+public class Level01 {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = 0;
+        int b = 0;
+
+        do{
+            a = sc.nextInt();
+            b = sc.nextInt();
+            if (a > 0 && b > 0) {
+                System.out.println(a+b);
+            }
+        }while(a > 0 && b > 0);
+
+        sc.close();
+
+    }
+}
+
